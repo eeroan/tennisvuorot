3bcc8a0aa41cb3e1bd97959df5f64f38352ed528
Eero Anttila <eea@iki.fi>
2015-08-28 16:29:08 +0300

    Add function for fetching availability for Meilahti

--- src/crawler.js
+++ src/crawler.js
@@ -1,6 +1,18 @@
 var url = require('url')
+var request = require('request')
+var Bacon = require('baconjs').Bacon
 
-module.exports = {table: table}
+module.exports = {
+    getMeilahti: getMeilahti,
+    table:       table
+}
+
+//Format: 2015-08-28
+function getMeilahti(isoDate) {
+    return Bacon.fromNodeCallback(request.get, {
+        url: 'https://www.slsystems.fi/meilahti/ftpages/ft-varaus-table-01.php?laji=1&pvm=' + isoDate + '&goto=0'
+    }).map('.body').map(table)
+}
 
 function table(html) {
     return html.match(/res=[^"]+/g).map(function (el) {
