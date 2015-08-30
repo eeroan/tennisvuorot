4182c027188a3fb82e2470576e28251cbd18cc61
Eero Anttila <eea@iki.fi>
2015-08-30 15:34:31 +0300

    Add sorting

--- public/front.js
+++ public/front.js
@@ -1,12 +1,12 @@
 $.getJSON('/courts', function (allData) {
     var data = [].concat(allData.meilahti, allData.herttoniemi)
-    var sameDates = _.map(_.groupBy(data, 'date'), objectToArray)
+    var sameDates = _.sortBy(_.map(_.groupBy(data, 'date'), objectToArray), 'key')
 
     $('.schedule').html(sameDates.map(function (obj) {
         var isoDate = obj.key
         var times = obj.val
         return '<h4>' + new Date(isoDate).toDateString() + '</h4>' +
-        _.map(_.groupBy(times, 'time'), objectToArray).map(function (obj2) {
+        _.sortBy(_.map(_.groupBy(times, 'time'), objectToArray), 'key') .map(function (obj2) {
             var isoTime = obj2.key
             var fields = obj2.val
             return '<p><span class="time">' + isoTime + '</span>' +
