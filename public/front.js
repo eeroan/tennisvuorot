fa6c36ead9dd331eac59a6e7f7fd240ab615b764
Eero Anttila <eea@iki.fi>
2015-08-30 15:38:49 +0300

    Rename variables

--- public/front.js
+++ public/front.js
@@ -2,13 +2,13 @@ $.getJSON('/courts', function (allData) {
     var data = [].concat(allData.meilahti, allData.herttoniemi)
     var sameDates = _.sortBy(_.map(_.groupBy(data, 'date'), objectToArray), 'key')
 
-    $('.schedule').html(sameDates.map(function (obj) {
-        var isoDate = obj.key
-        var times = obj.val
+    $('.schedule').html(sameDates.map(function (dateObject) {
+        var isoDate = dateObject.key
+        var times = dateObject.val
         return '<h4>' + new Date(isoDate).toDateString() + '</h4>' +
-        _.sortBy(_.map(_.groupBy(times, 'time'), objectToArray), 'key') .map(function (obj2) {
-            var isoTime = obj2.key
-            var fields = obj2.val
+        _.sortBy(_.map(_.groupBy(times, 'time'), objectToArray), 'key') .map(function (timeObject) {
+            var isoTime = timeObject.key
+            var fields = timeObject.val
             return '<p><span class="time">' + isoTime + '</span>' +
             fields.map(function (field) {
                 return '<button type="button" class="btn btn-' +
