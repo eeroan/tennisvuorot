b6d9b72ffce0b9b2876b1bb92987b94ec6c6b459
Eero Anttila <eea@iki.fi>
2015-08-31 23:37:16 +0300

    Add missing information of tennis courts

--- public/front.js
+++ public/front.js
@@ -1,39 +1,52 @@
 var courts = {
     tali: {
         url: 'https://webtimmi.talintenniskeskus.fi/login.do?loginName=GUEST&password=GUEST',
-        address: 'Kutomokuja 4, 00381 Helsinki'
+        address: 'Kutomokuja 4, 00381 Helsinki',
+        tel: '09 5656050'
     },
     taivallahti: {
         url: 'https://webtimmi.talintenniskeskus.fi/login.do?loginName=GUEST&password=GUEST',
-        address: 'Hiekkarannantie 2, 00100 Helsinki'
+        address: 'Hiekkarannantie 2, 00100 Helsinki',
+        tel: '09 4770490'
     },
     meilahti: {
         url: 'https://www.slsystems.fi/meilahti/',
-        address: 'Meilahden Liikuntapuisto, 00250 Helsinki'
+        address: 'Meilahden Liikuntapuisto, 00250 Helsinki',
+        tel: '050 3748068'
     },
     puistola: {
         url: 'https://oma.enkora.fi/tapanila/reservations2/reservations/25/-/-/-',
-        address: 'Tapulikaupungintie 4, 00750 Helsinki'
+        address: 'Tapulikaupungintie 4, 00750 Helsinki',
+        tel: '09 3462511'
     },
     herttoniemi: {
         url: 'https://www.slsystems.fi/fite/',
-        address: 'Varikkotie 4, 00900 Helsinki'
+        address: 'Varikkotie 4, 00900 Helsinki',
+        tel: '09 341 7130'
     },
     kulosaari: {
         url: 'http://www.slsystems.fi/puhoscenter/',
-        address: 'Kulosaarentie 2, 00570 Helsinki'
+        address: 'Kulosaarentie 2, 00570 Helsinki',
+        tel: '09 6211303'
     },
     viikki: {
-        tel: '02941 58702'
+        tel: '02941 58702',
+        address: 'Maakaari 3, 00790 Helsinki'
     },
     merihaka: {
-        url: 'https://www.slsystems.fi/meripeli/'
+        url: 'https://www.slsystems.fi/meripeli/',
+        address: 'Haapaniemenkatu 14 B, 00530 Helsinki',
+        tel: '01043 97 979'
     },
     esport: {
-        url: 'http://varaus.esportcenter.fi/index.php?pageId=11&func=mod_rc_v2&tac='
+        url: 'http://varaus.esportcenter.fi/index.php?pageId=11&func=mod_rc_v2&tac=',
+        address: 'Koivu-Mankkaan tie 5, 02200 Espoo',
+        tel: '09 502 4700'
     },
     hiekkaharju: {
-        url: 'https://www.slsystems.fi/hiekkaharjuntenniskeskus/'
+        url: 'https://www.slsystems.fi/hiekkaharjuntenniskeskus/',
+        address: 'Tennistie 5, 01370 Vantaa',
+        tel:'09 8731923'
     },
     kalastajatorppa: {
         tel: '010 423 9960',
@@ -41,7 +54,8 @@ var courts = {
     },
     laajasalo: {
         url: 'http://www.slsystems.fi/laajasalonpalloiluhallit/',
-        address: 'Sarvastonkaari 23, 00840 Helsinki'
+        address: 'Sarvastonkaari 23, 00840 Helsinki',
+        tel: '09 6987654'
     },
     varmatennis: {
         tel: '+358 9 548 6101',
