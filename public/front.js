add13d4f53976dc8e07d5cb84ec455b248e77f94
Eero Anttila <eea@iki.fi>
2015-09-06 18:32:18 +0300

    Extrat map operations to named functions

--- public/front.js
+++ public/front.js
@@ -12,29 +12,7 @@ $('.toggleInformation').click(function () {
 
 $.getJSON('/courts', function (allData) {
     var data = [].concat(allData.meilahti, allData.herttoniemi)
-    $('.schedule').html(
-        groupBySortedAsList(data, 'date').map(function (dateObject) {
-        var isoDate = dateObject.key
-        var times = dateObject.val
-        return '<h4>' + new Date(isoDate).toDateString() + '</h4>' +
-            groupBySortedAsList(times, 'time').map(function (timeObject) {
-                var isoTime = timeObject.key
-                var fields = timeObject.val
-                return '<p><span class="time">' + isoTime + '</span>' +
-                    groupBySortedAsList(fields, 'location').map(function (locationFields) {
-                        var location = locationFields.key
-                        var fields = locationFields.val
-                        return '<span class="locationBoxes"><button type="button" class="locationLabel btn btn-' +
-                            (location === 'meilahti' ? 'primary' : 'success') +
-                            ' btn-xs">' + location + ' (' + fields.length + ')</button>' +
-                            fields.map(function (field) {
-                                return '<button type="button" class="fieldLabel btn btn-' +
-                                    (field.location === 'meilahti' ? 'primary' : 'success') +
-                                    ' btn-xs">' + field.field + '</button>'
-                            }).join('') + '</span>'
-                    }).join('') + '</p>'
-            }).join('')
-    }).join(''))
+    $('.schedule').html(groupBySortedAsList(data, 'date').map(toDateSection).join(''))
 })
 
 $('.schedule').on('click', '.locationLabel', function (e) {
@@ -43,6 +21,35 @@ $('.schedule').on('click', '.locationLabel', function (e) {
     $locationLabel.toggle()
     $locationLabel.parent().find('.fieldLabel').toggle()
 })
+
+function toDateSection(dateObject) {
+    var isoDate = dateObject.key
+    var times = dateObject.val
+    return '<h4>' + new Date(isoDate).toDateString() + '</h4>' +
+        groupBySortedAsList(times, 'time').map(toTimeRow).join('')
+}
+
+function toTimeRow(timeObject) {
+    var isoTime = timeObject.key
+    var fields = timeObject.val
+    return '<p><span class="time">' + isoTime + '</span>' +
+        groupBySortedAsList(fields, 'location').map(toLocationButtonGroup).join('') + '</p>'
+}
+
+function toLocationButtonGroup(locationFields) {
+    var location = locationFields.key
+    var fields = locationFields.val
+    return '<span class="locationBoxes"><button type="button" class="locationLabel btn btn-' +
+        (location === 'meilahti' ? 'primary' : 'success') +
+        ' btn-xs">' + location + ' (' + fields.length + ')</button>' +
+        fields.map(toButtonMarkup).join('') + '</span>'
+}
+
+function toButtonMarkup(field) {
+    return '<button type="button" class="fieldLabel btn btn-' +
+        (field.location === 'meilahti' ? 'primary' : 'success') +
+        ' btn-xs">' + field.field + '</button>'
+}
 
 function groupBySortedAsList(list, key) {
     return _.sortBy(_.map(_.groupBy(list, key), objectToArray), 'key')
