322484aba0919cd6252f31791634a1588b6c37fa
Eero Anttila <eea@iki.fi>
2015-09-06 23:05:58 +0300

    Clean up some code

--- public/front.js
+++ public/front.js
@@ -16,68 +16,41 @@ $('.schedule').on('click', '.locationLabel', function (e) {
 
 function initialize() {
     $.getJSON('/locations', function (locations) {
-
         $('.information tbody').html(locations.map(function (obj) {
-            var address = obj.address;
-            var url = obj.url;
-            var title = obj.title;
-            var tel = obj.tel;
+            var address = obj.address
+            var url = obj.url
+            var title = obj.title
+            var tel = obj.tel
             return '<tr><td class="place">' + (url ? '<a target="_blank" href="' + url + '">' + title + '</a>' : title) + '</td>' + '<td><a target="_blank" href="http://maps.google.com/?q=' + address + '">' + address + '</a></td>' +
                 '<td><a href="tel:' + tel + '">' + tel + '</a></td></tr>'
         }).join(''))
-
-        var map
         var bounds = new google.maps.LatLngBounds()
-        var mapOptions = {
-            mapTypeId: 'roadmap'
-        };
-
-        // Display a map on the page
-        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        var map = new google.maps.Map(document.getElementById("map_canvas"), { mapTypeId: 'roadmap'})
         map.setTilt(45)
-
-        // Multiple Markers
-        var locations2 = [
-            {title: 'London Eye, London', lat: 51.503454, lng: -0.119562, info:'<div class="info_content">' +
-            '<h3>London Eye</h3>' +
-            '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' + '</div>'},
-            {title:'Palace of Westminster, London', lat: 51.499633,lng: -0.124755, info:'<div class="info_content">' +
-            '<h3>Palace of Westminster</h3>' +
-            '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
-            '</div>'}
-        ];
-
-        // Display multiple markers on a map
         var infoWindow = new google.maps.InfoWindow()
-
-        locations2.forEach(function(loc, i) {
-        // Loop through our array of markers & place each one on the map
-            var position = new google.maps.LatLng(loc.lat, loc.lng);
-            bounds.extend(position);
+        locations.filter(function(loc) {return loc.lat !== null}).forEach(function(loc, i) {
+            var position = new google.maps.LatLng(loc.lat, loc.lng)
+            bounds.extend(position)
             var marker = new google.maps.Marker({
                 position: position,
                 map: map,
                 title: loc.title
-            });
-
-            // Allow each marker to have an info window
+            })
             google.maps.event.addListener(marker, 'click', (function (marker, i) {
                 return function () {
-                    infoWindow.setContent(loc.info);
-                    infoWindow.open(map, marker);
+                    infoWindow.setContent(locationTooltip(loc))
+                    infoWindow.open(map, marker)
                 }
-            })(marker, i));
-
-            // Automatically center the map fitting all markers on the screen
-            map.fitBounds(bounds);
+            })(marker, i))
+            map.fitBounds(bounds)
         })
-
-        // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-        var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function (event) {
-            this.setZoom(14);
-            google.maps.event.removeListener(boundsListener);
-        });
     })
+}
+
+function locationTooltip(location) {
+    return '<h3>' + location.title + '</h3><p><a href="'+location.url+'">Varausjärjestelmä</a></p>'+
+            '<p>'+location.address+'</p>'+
+            '<p><a href="tel:'+location.tel+'">'+location.tel+'</a></p>'
 }
 
 function toDateSection(dateObject) {
