1b442420a5169dd8c47a45dc84b9b998bb329258
Eero Anttila <eea@iki.fi>
2015-09-15 14:58:27 +0300

    Hard code locations to front code

--- public/front.js
+++ public/front.js
@@ -32,7 +32,21 @@ $('.schedule').on('click', '.locationLabel', function (e) {
     $locationLabel.toggle()
     $locationLabel.parent().find('.fieldLabel').toggle()
 })
-$.getJSON('/locations', function (locations) {
+
+//$.getJSON('/locations', renderLocations)
+
+renderLocations(locations())
+
+function todayIsoDate(delta) {
+    var now = new Date()
+    if (delta) {
+        now.setDate(now.getDate() + delta)
+    }
+    var isoDateTime = now.toISOString()
+    return isoDateTime.split('T')[0]
+}
+
+function renderLocations(locations) {
     window.locations = locations
     $('.information tbody').html(locations.map(function (obj) {
         var address = obj.address
@@ -43,15 +57,6 @@ $.getJSON('/locations', function (locations) {
             '<td><a target="_blank" href="http://maps.google.com/?q=' + address + '">' + address + '</a></td>' +
             '<td><a href="tel:' + tel + '">' + tel + '</a></td></tr>'
     }).join(''))
-})
-
-function todayIsoDate(delta) {
-    var now = new Date()
-    if (delta) {
-        now.setDate(now.getDate() + delta)
-    }
-    var isoDateTime = now.toISOString()
-    return isoDateTime.split('T')[0]
 }
 
 function renderMap() {
@@ -88,7 +93,7 @@ function locationTooltip(location) {
 function toDateSection(dateObject) {
     var isoDate = dateObject.key
     var times = dateObject.val
-    return '<h4>' + DateFormat.format(DateTime.fromIsoDate(isoDate), DateFormat.patterns.FiWeekdayDatePattern,  DateLocale.FI) + '</h4>' +
+    return '<h4>' + DateFormat.format(DateTime.fromIsoDate(isoDate), DateFormat.patterns.FiWeekdayDatePattern, DateLocale.FI) + '</h4>' +
         groupBySortedAsList(times, 'time').map(toTimeRow).join('')
 }
 
@@ -118,4 +123,94 @@ function groupBySortedAsList(list, key) {
 
 function objectToArray(val, key) {
     return {key: key, val: val}
+}
+
+function locations() {
+    return [{
+        "title":   "tali",
+        "lat":     60.2134843,
+        "lng":     24.876687,
+        "url":     "https://webtimmi.talintenniskeskus.fi/login.do?loginName=GUEST&password=GUEST",
+        "address": "Kutomokuja 4, 00381 Helsinki",
+        "tel":     "09 5656050"
+    }, {
+        "title":   "taivallahti",
+        "lat":     60.1728402,
+        "lng":     24.9062133,
+        "url":     "https://webtimmi.talintenniskeskus.fi/login.do?loginName=GUEST&password=GUEST",
+        "address": "Hiekkarannantie 2, 00100 Helsinki",
+        "tel":     "09 4770490"
+    }, {
+        "title":   "meilahti",
+        "lat":     60.19008059999999,
+        "lng":     24.8972334,
+        "url":     "https://www.slsystems.fi/meilahti/",
+        "address": "Meilahden Liikuntapuisto, 00250 Helsinki",
+        "tel":     "050 3748068"
+    }, {
+        "title":   "puistola",
+        "lat":     60.2706335,
+        "lng":     25.033526,
+        "url":     "https://oma.enkora.fi/tapanila/reservations2/reservations/25/-/-/-",
+        "address": "Tapulikaupungintie 4, 00750 Helsinki",
+        "tel":     "09 3462511"
+    }, {
+        "title":   "herttoniemi",
+        "lat":     60.2089734,
+        "lng":     25.0682377,
+        "url":     "https://www.slsystems.fi/fite/",
+        "address": "Varikkotie 4, 00900 Helsinki",
+        "tel":     "09 341 7130"
+    }, {
+        "title":   "kulosaari",
+        "lat":     60.1859158,
+        "lng":     25.0006762,
+        "url":     "http://www.slsystems.fi/puhoscenter/",
+        "address": "Kulosaarentie 2, 00570 Helsinki",
+        "tel":     "09 6211303"
+    }, {
+        "title":   "viikki",
+        "lat":     60.23024509999999,
+        "lng":     25.0249103,
+        "tel":     "02941 58702",
+        "address": "Maakaari 3, 00790 Helsinki"
+    }, {
+        "title":   "merihaka",
+        "lat":     60.17964199999999,
+        "lng":     24.9629867,
+        "url":     "https://www.slsystems.fi/meripeli/",
+        "address": "Haapaniemenkatu 14 B, 00530 Helsinki",
+        "tel":     "01043 97 979"
+    }, {
+        "title":   "esport",
+        "lat":     60.1767394,
+        "lng":     24.7814655,
+        "url":     "http://varaus.esportcenter.fi/index.php?pageId=11&func=mod_rc_v2&tac=",
+        "address": "Koivu-Mankkaan tie 5, 02200 Espoo",
+        "tel":     "09 502 4700"
+    }, {
+        "title":   "hiekkaharju",
+        "lat":     60.3043802,
+        "lng":     25.0522693,
+        "url":     "https://www.slsystems.fi/hiekkaharjuntenniskeskus/",
+        "address": "Tennistie 5, 01370 Vantaa",
+        "tel":     "09 8731923"
+    }, {"title": "kalastajatorppa", "tel": "010 423 9960", "address": "Kärkitie 4, 00330 Helsinki"}, {
+        "title":   "laajasalo",
+        "url":     "http://www.slsystems.fi/laajasalonpalloiluhallit/",
+        "address": "Sarvastonkaari 23, 00840 Helsinki",
+        "tel":     "09 6987654"
+    }, {
+        "title":   "varmatennis",
+        "tel":     "+358 9 548 6101",
+        "address": "Ruosilankuja 12, 00390 Helsinki"
+    }, {"title": "metsälä", "address": "Krämertintie 6, 00620 Helsinki", "tel": "09 798 521"}, {
+        "title":   "kaisaniemi",
+        "lat":     60.1738527,
+        "lng":     24.9442649,
+        "url":     "https://www.slsystems.fi/fite/",
+        "address": "Kaisaniemen puisto",
+        "tel":     "09 341 7130"
+    }]
+
 }
