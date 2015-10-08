deb5e6eff536940c80bcd9a264cbf81790345588
Eero Anttila <eea@iki.fi>
2015-10-08 21:03:59 +0300

    Change days

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 var DateTime = require('dateutils').DateTime
 var cache = require('./cache')
-cache.update(DateTime.today())
-cache.update(DateTime.today().plusDays(1))
+cache.update(DateTime.today().plusDays(7))
+cache.update(DateTime.today().plusDays(8))
