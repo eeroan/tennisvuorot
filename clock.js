8fb00f7f09b2033cd843af9aeaa85d4f4436d89c
Eero Anttila <eea@iki.fi>
2015-10-10 10:58:39 +0300

    Fix error

--- clock.js
+++ clock.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 var DateTime = require('dateutils').DateTime
 var dao = require('./src/dao')
+
+doRefresh()
 setInterval(function () {
-    dao.refresh(DateTime.today().toISODateString(), function (done) {
-        dao.refresh(DateTime.today().plusDays(1).toISODateString())
-    })
+    doRefresh()
 }, 1000*60*60)
+
+function doRefresh() {
+    dao.refresh(DateTime.today().toISODateString(), function (done) {
+        dao.refresh(DateTime.today().plusDays(1).toISODateString(), function () { })
+    })
+}
+
+
