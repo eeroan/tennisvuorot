b5fce279f91c32ce367818816e2874d3a9999f5c
Eero Anttila <eea@iki.fi>
2015-10-20 10:27:15 +0300

    Remove forgotten route

--- src/router.js
+++ src/router.js
@@ -6,6 +6,5 @@ var dao = require('./dao')
 route.use('/front.min.js', browserify(__dirname + '/../public/front.js'))
 route.use(express.static(__dirname + '/../public'))
 route.get('/courts', dao.freeCourts)
-route.get('/locations', dao.locations)
 
 module.exports = route
