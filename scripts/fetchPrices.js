963a96a82f5b588aef6fce80224ecfc44d46a2f7
Eero Anttila <eea@iki.fi>
2015-10-29 15:19:28 +0200

    Put prices to single file and add script to package.json

--- scripts/fetchPrices.js
+++ scripts/fetchPrices.js
@@ -3,6 +3,7 @@ var Bacon = require('baconjs').Bacon
 var request = require('request')
 var util = require('util')
 var fs = require('fs')
+var _ = require('lodash')
 //Url for editing: https://docs.google.com/spreadsheets/d/1TwYmdHhGnB0RZh75bRdHaJy9erXTJpsVxCvCX0SX8f4/edit
 var url = 'https://docs.google.com/spreadsheets/d/1TwYmdHhGnB0RZh75bRdHaJy9erXTJpsVxCvCX0SX8f4/pub?output=csv&gid='
 var sheets = {
@@ -12,9 +13,9 @@ var sheets = {
     meilahti:    466490005
 }
 
-function fetchFor(name) {
+function fetchFor(id) {
     return Bacon.fromNodeCallback(request.get, {
-        url: url + sheets[name]
+        url: url + id
     }).map('.body').map(function (csv) {
         var list = csv.split('\r\n').splice(1)
         var obj = {}
@@ -23,12 +24,20 @@ function fetchFor(name) {
             obj[row[0]] = row.splice(1)
         })
         return obj
-    }).onValue(function (data) {
-        console.log('Writing to ' + name + '.json')
-        fs.writeFileSync(__dirname + '/../src/' + name+'Prices.js','module.exports = ' + util.inspect(data, {colors: false, depth: null}))
     })
 }
 
+function fetchAll() {
+    var fileName = 'rates.js'
+    Bacon.combineTemplate(_.mapValues(sheets, fetchFor))
+        .onValue(function (data) {
+            console.log('Writing rates to to ' + fileName)
+            fs.writeFileSync(__dirname + '/../src/' + fileName, 'module.exports = ' + util.inspect(data, {
+                    colors: false,
+                    depth:  null
+                }))
+        })
+}
 var args = process.argv.splice(2)
 
-fetchFor(args[0])
+fetchAll()
