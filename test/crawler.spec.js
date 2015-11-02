cc7a4a4f3c96552145bf1e9d9ed10e1a8b2c582c
Eero Anttila <eea@iki.fi>
2015-11-02 13:32:37 +0200

    Convert tests to es6 format

--- test/crawler.spec.js
+++ test/crawler.spec.js
@@ -2,28 +2,28 @@ var fs = require('fs')
 var slSystems = require('../src/slSystemsCrawler')
 var webTimmi = require('../src/webTimmiCrawler')
 
-describe('sls systems crawler', function () {
+describe('sls systems crawler', () => {
     var obj
     var expected
-    before(function () {
+    before(() => {
         obj = slSystems.table(fs.readFileSync(__dirname + '/fixture/meilahti.html', 'utf-8'))
         expected = JSON.parse(fs.readFileSync(__dirname + '/fixture/meilahti.json', 'utf-8'))
     })
 
-    it('transforms table to json', function () {
+    it('transforms table to json', () => {
         expect(obj).to.eql(expected)
     })
 })
 
-describe('web timmi crawler', function () {
+describe('web timmi crawler', () => {
     var obj
     var expected
-    before(function () {
+    before(() => {
         obj = webTimmi.parseMarkup(fs.readFileSync(__dirname + '/fixture/weekViewMenu.html', 'utf-8'))
         expected = JSON.parse(fs.readFileSync(__dirname + '/fixture/taivallahti.json', 'utf-8'))
     })
 
-    it('transforms table to json', function () {
+    it('transforms table to json', () => {
         expect(obj).to.eql(expected)
     })
 })
