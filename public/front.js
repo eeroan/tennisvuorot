3ec6d9975d0e44375c588af1a94eb3ca1a02c1f1
Eero Anttila <eea@iki.fi>
2015-11-16 17:51:08 +0200

    Fix off by 1 bug again

--- public/front.js
+++ public/front.js
@@ -44,7 +44,7 @@ function loadMoreResults(days) {
 
 function listAvailabilityForActiveDate(days) {
     var requestedDate = activeDate.toISODateString()
-    activeDate = activeDate.plusDays(days-1)
+    activeDate = activeDate.plusDays(days)
     $('#schedule').addClass('loading')
     alreadyLoadingMoreResults = true
     return $.getJSON(`/courts?date=${requestedDate}&days=${days}&refresh=${window.refresh}`, allDataWithDates => {
