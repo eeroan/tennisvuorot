d589917bd3caefa4e740440e583add52df84d9b2
Eero Anttila <eea@iki.fi>
2015-11-19 13:36:20 +0200

    Permit force refresh for content got from backend

--- src/router.js
+++ src/router.js
@@ -13,18 +13,19 @@ route.use('/front.min.js', babelify(__dirname + '/front.js'))
 route.get('/courts', dao.sendFreeCourts)
 route.use(express.static(__dirname + '/../public'))
 route.get('/', (req, res) => {
+    var refresh = req.query.refresh === 'true'
     res.write(`<!DOCTYPE html>
         <html>`)
     res.write(headHtml())
     res.write(`<body><div class="container reservations detail">`)
     res.write(filtersHtml())
     res.write(`<section class="" id="schedule">`)
-    dao.freeCourts(new DateTime().toISODateString(), 3, false, (data) => {
+    dao.freeCourts(new DateTime().toISODateString(), 3, refresh, (data) => {
         res.write(markupForDateRange(data, new DateTime()))
         res.write('</section></div>')
         res.write(modalsHtml())
         res.write(scriptsHtml({
-            refresh:    req.query.refresh === 'true',
+            refresh:    refresh,
             serverDate: new DateTime().toISODateString()
         }))
         res.write('</body></html>')
