e646ddc499058b130a7fe03c3f42a671c11b2753
Eero Anttila <eea@iki.fi>
2015-11-24 10:41:30 +0200

    Use with statement

--- src/filters.html.js
+++ src/filters.html.js
@@ -1,15 +1,15 @@
-module.exports = o => `<div class="filters">
+module.exports = o => { with(o) return `<div class="filters">
         <a href="javascript:void(0)" class="toggleFilters" title="Näytä hakuehdot"></a>
 
         <div class="locationsContainer">
             <a href="javascript:void(0)" class="toggle toggleMapInformation"></a>
             <a href="javascript:void(0)" class="toggle toggleInformation">i</a>
-            ${o.places.map(place => `
+            ${places.map(place => `
             <button type="button" class="${place.id}" id="${place.id}">${place.name}</button>
             `).join('')}
         </div>
         <div class="fieldTypeContainer">
-            ${o.types.map(type => `
+            ${types.map(type => `
             <button type="button" class="${type.id}" id="${type.id}">${type.name}</button>
             `).join('')}
             <label><input id="single" type="checkbox"> Vapaana 2h</label>
@@ -23,4 +23,4 @@ module.exports = o => `<div class="filters">
             <select class="jumpToDate"></select>
         </div>
         <div class="close" title="Piilota">&times;</div>
-    </div>`
+    </div>`}
