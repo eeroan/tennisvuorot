c7b9a09e27880bd6bad9c4e6b04987908b932937
Eero Anttila <eea@iki.fi>
2015-12-26 16:56:51 +0200

    Add link to history

--- src/modals.html.js
+++ src/modals.html.js
@@ -1,9 +1,11 @@
 module.exports = o => `<div class="information modal">
     <h3>Tietoja</h3>
     <p>Tämän palvelun on tehnyt Eero Anttila harrasteprojektinaan. </p>
-    <p>Lähdekoodi: <a href="https://github.com/eeroan/tennisvuorot">GitHub</a></p>
-    <p>Twitter: <a href="https://twitter.com/eeroan">@eeroan</a></p>
-    <p>Töissä: <a href="http://reaktor.com">Reaktorilla</a></p>
+    <p>Katso myös <a href="/historia" onclick="ga('send', 'event', 'History', 'navigate'); return true;">Tilastot</a></p>
+    <p>
+    Lähdekoodi: <a href="https://github.com/eeroan/tennisvuorot">GitHub</a>
+    <br>Twitter: <a href="https://twitter.com/eeroan">@eeroan</a>
+    <br>Töissä: <a href="http://reaktor.com">Reaktorilla</a></p>
     <form class="feedbackForm">
     <textarea class="feedback" placeholder="Tähän voit antaa palautetta ja toiveita"></textarea>
     <button type="submit" class="submitFeedback">Lähetä</button>
