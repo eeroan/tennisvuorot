89fcf8a7a758f29adfe041d2ed95b88d2a59a6d1
Eero Anttila <eea@iki.fi>
2015-12-26 16:07:53 +0200

    Remove redundant file

--- scripts/printHistory.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const util = require('util')
-const history = require('../src/history')
-
-var data = history.availabilityByDate()
-console.log(util.inspect(data, {
-    colors: true,
-    depth:  null
-}))
