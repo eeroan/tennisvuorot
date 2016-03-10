b1893b64eb106581559cd7ebf9f91ba0956ec1d8
Eero Anttila <eea@iki.fi>
2016-03-10 22:51:16 +0200

    Update two upcoming days in every 10 minutes

--- worker.js
+++ worker.js
@@ -2,18 +2,24 @@ process.env.NEW_RELIC_ENABLED = true
 require('newrelic')
 var DateTime = require('dateutils').DateTime
 var dao = require('./src/dao/dao')
-var daysAhead = 0
-var maxDaysAhead = 60
 
-doRefresh()
-setInterval(() => {
+updateReservations(2)
+setTimeout(() => updateReservations(60), 1000 * 60 * 2)
+
+function updateReservations(days) {
+    var daysAhead = 0
+    var maxDaysAhead = days
+
     doRefresh()
-}, 1000 * 60 * 5)
+    setInterval(() => {
+        doRefresh()
+    }, 1000 * 60 * 5)
 
-function doRefresh() {
-    dao.refresh(new DateTime().plusDays(daysAhead).toISODateString(), 1, () => { })
-    if (daysAhead > maxDaysAhead) daysAhead = 0
-    else daysAhead++
+    function doRefresh() {
+        dao.refresh(new DateTime().plusDays(daysAhead).toISODateString(), 1, () => { })
+        if (daysAhead > maxDaysAhead) daysAhead = 0
+        else daysAhead++
+    }
 }
 
 
