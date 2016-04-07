99e4b4ff20b97a5aadd49570d150b4dd806ac90b
Eero Anttila <eea@iki.fi>
2016-04-07 23:44:44 +0300

    Add titles in search panel

--- src/mainPage/filters.html.js
+++ src/mainPage/filters.html.js
@@ -1,21 +1,24 @@
 module.exports = o => { with(o) return `<div class="filters">
         <a href="javascript:void(0)" class="toggleFilters" title="Näytä hakuehdot"></a>
 
+        <h3>Tennispaikat <a href="javascript:void(0)" class="toggle toggleMapInformation">Näytä kartalla</a></h3> 
         <div class="locationsContainer">
-            <a href="javascript:void(0)" class="toggle toggleMapInformation"></a>
             ${places.map(place => `
             <button type="button" class="button ${place.id}" id="${place.id}">${place.name}</button>
             `).join('')}
         </div>
+        <h3>Kentän tyyppi</h3>
         <div class="fieldTypeContainer">
             ${types.map(type => `
             <button type="button" class="button ${type.id}" id="${type.id}">${type.name}</button>
             `).join('')}
-            <label><input id="single" type="checkbox"> Vapaana 2h</label>
         </div>
+        <h3>Aikavalinnat</h3>
         <div class="timeFilterContainer">
             <div id="slider"></div>
             <span class="rangeLabel">6:00-23:00</span>
+            <label><input id="single" type="checkbox"> Vapaana&nbsp;2h</label>
         </div>
+        
         <div class="close" title="Piilota">&times;</div>
     </div>`}
