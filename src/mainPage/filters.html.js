6012c40728109246abe4b89206206f1c0862a424
Eero Anttila <eea@iki.fi>
2016-04-11 23:10:20 +0300

    Show quick links

--- src/mainPage/filters.html.js
+++ src/mainPage/filters.html.js
@@ -1,7 +1,7 @@
-module.exports = o => { with(o) return `<div class="filters">
+module.exports = o => { with(o) return `<div class="filters"><div class="filtersWrapper">
         <a href="javascript:void(0)" class="toggleFilters" title="Näytä hakuehdot"></a>
 
-        <h3>Tennispaikat <a href="javascript:void(0)" class="toggle toggleMapInformation">Näytä kartalla</a></h3> 
+        <h3>Tennispaikat <a href="javascript:void(0)" class="toggle toggleMapInformation">Näytä kartalla</a></h3>
         <div class="locationsContainer">
             ${places.map(place => `
             <button type="button" class="button ${place.id}" id="${place.id}">${place.name}</button>
@@ -19,6 +19,7 @@ module.exports = o => { with(o) return `<div class="filters">
             <span class="rangeLabel">6:00-23:00</span>
             <label><input id="single" type="checkbox"> Vapaana&nbsp;2h</label>
         </div>
-        
+
         <div class="close" title="Piilota">&times;</div>
+        </div>
     </div>`}
