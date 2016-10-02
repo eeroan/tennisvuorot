4b30797172c3720f8b30522443def1ac3fcf6fc0
Eero Anttila <eea@iki.fi>
2016-10-02 13:30:52 +0300

    Refactor

--- src/dao/webTimmi2.js
+++ src/dao/webTimmi2.js
@@ -3,75 +3,39 @@ const request = require('request')
 const Bacon = require('baconjs').Bacon
 const format = require('../format')
 
-const login = () =>
-    Bacon.fromNodeCallback(request.get, {
-        url: 'https://timmi.talintenniskeskus.fi/WebTimmi/login.do?loginName=GUEST&password=GUEST'
-    }).flatMap(res => {
-        try {
-            return res.headers['set-cookie'][0].split(';')[0]
-        } catch (e) {
-            return new Bacon.Error(e)
-        }
-    })
-
-const getWeek = cookie =>
-    Bacon.fromNodeCallback(request.get, {
-        url: 'https://timmi.talintenniskeskus.fi/WebTimmi/weekViewAjaxAction.do?oper=getItems',
+const req = (method, url, cookie, form) => {
+    var opts = {
+        url: `https://timmi.talintenniskeskus.fi/WebTimmi/${url}`,
         headers: {
             Cookie: cookie,
             'Cache-Control': 'no-cache'
         }
-    }).map('.body').map(JSON.parse)
+    }
+    if(cookie) opts.headers = {Cookie: cookie}
+    if(form) opts.form = form
+    return Bacon.fromNodeCallback(request[method], opts)
+}
+const get = (url, cookie, form) => req('get', url, cookie, form)
+const post = (url, cookie, form) => req('post', url, cookie, form)
+const json = observable => observable.map('.body').map(JSON.parse)
 
-const getProfiles = cookie =>
-    Bacon.fromNodeCallback(request.post, {
-        url: 'https://timmi.talintenniskeskus.fi/WebTimmi/autoCompleteAjax.do',
-        headers: {
-            Cookie: cookie
-        },
-        form: {
-            actionCode: 'getProfiles'
-        }
-    }).map('.body').map(JSON.parse)
+const login = () => get('login.do?loginName=GUEST&password=GUEST').flatMap(res => {
+    try {
+        return res.headers['set-cookie'][0].split(';')[0]
+    } catch (e) {
+        return new Bacon.Error(e)
+    }
+})
 
-const getRoomParts = cookie =>
-    Bacon.fromNodeCallback(request.post, {
-        url: 'https://timmi.talintenniskeskus.fi/WebTimmi/getRoomPartsForCalendarAjax.do',
-        headers: {
-            Cookie: cookie
-        },
-        form: {
-            actionCode: 'getRoomPartsForProfile',
-            id: 3
-        }
-    }).map('.body').map(JSON.parse)
-
-const getWeekView = cookie =>
-    Bacon.fromNodeCallback(request.post, {
-        url: 'https://timmi.talintenniskeskus.fi/WebTimmi/weekViewAjaxAction.do',
-        headers: {
-            Cookie: cookie
-        },
-        form: {
-            oper: 'getRightsResourcesForCalendar'
-        }
-    }).map('.body').map(JSON.parse)
-
-const getTimeCells = cookie =>
-    Bacon.fromNodeCallback(request.get, {
-        url: 'https://timmi.talintenniskeskus.fi/WebTimmi/weekViewAjaxAction.do?oper=getTimeCells&_=1475315219135',
-        headers: {
-            Cookie: cookie
-        }
-    }).map('.body').map(JSON.parse)
-
-const getTimeZone = cookie =>
-    Bacon.fromNodeCallback(request.get, {
-        url: 'https://timmi.talintenniskeskus.fi/WebTimmi/timeZoneAjax.do',
-        headers: {
-            Cookie: cookie
-        }
-    }).map('.body').map(JSON.parse)
+const getWeek = cookie => json(get('weekViewAjaxAction.do?oper=getItems', cookie))
+const getProfiles = cookie => json(post('autoCompleteAjax.do', cookie, {actionCode: 'getProfiles'}))
+const getRoomParts = cookie => json(post('getRoomPartsForCalendarAjax.do', cookie, {
+    actionCode: 'getRoomPartsForProfile',
+    id: 3
+}))
+const getWeekView = cookie => json(post('weekViewAjaxAction.do', cookie, { oper: 'getRightsResourcesForCalendar'}))
+const getTimeCells = cookie => json(get('weekViewAjaxAction.do?oper=getTimeCells', cookie))
+const getTimeZone = cookie => json(get('timeZoneAjax.do', cookie))
 
 //login().flatMap(getProfiles).onValue(x => console.log('jee', format.prettyPrint(x)))
 //login().flatMap(getRoomParts).map(format.prettyPrint).log()
