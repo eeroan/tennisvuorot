6e0dbe82cf5a0729e837384bb02babd7279b7a0e
Eero Anttila <eea@iki.fi>
2016-10-02 14:14:52 +0300

    Simplify api call

--- src/dao/webTimmi2.js
+++ src/dao/webTimmi2.js
@@ -38,28 +38,28 @@ const updateStructure = cookie => post('weekViewAjaxAction.do', cookie, {
     oper: 'updateStructure',
     structure: JSON.stringify({
         "structure": [{
-            "roomPartIds": ["5741", "5742", "5743", "5744", "5745", "5746", "5799", "5800", "5846", "5847", "5848", "5823"],
-            "roomPartNames": ["TK 13", "TK 10", "TK 9", "TK 8", "TK 1", "TK 2", "TK 14", "TK 15", "TK 20", "TK 21", "TK 22", "HUOM"],
-            "roomPartColors": ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"],
-            "startTime": "06:30",
-            "endTime": "22:30",
-            "periodization": "60",
-            "calendarSize": 4,
-            "selectedDays": [0, 0, 0, 0, 0, 0, 1],
-            "singlePickedDates": false,
-            "referenceDateMills": 1474885798000
+            roomPartIds: ['5741', '5742', '5743', '5744', '5745', '5746', '5799', '5800', '5846', '5847', '5848', '5823'],
+            roomPartNames: [],
+            roomPartColors: [],
+            startTime: '06:30',
+            endTime: '22:30',
+            periodization: '60',
+            calendarSize: 4,
+            selectedDays: [0, 0, 0, 0, 0, 0, 1],
+            singlePickedDates: false,
+            referenceDateMills: 1474885798000
         }]
     }),
     form: JSON.stringify({
-        "sundaySelected": "1",
-        "startTime": "06:30",
-        "endTime": "22:30",
-        "timePeriod": "60",
-        "copyMoveDefault": "0",
-        "copyMoveOption": "0",
-        "lockTimeOnChoose": "1",
-        "minTimeChange": "15",
-        "calendarSize": "4"
+        sundaySelected: '1',
+        startTime: '06:30',
+        endTime: '22:30',
+        timePeriod: '60',
+        copyMoveDefault: '0',
+        copyMoveOption: '0',
+        lockTimeOnChoose: '1',
+        minTimeChange: '15',
+        calendarSize: '4'
     })
 })
 const getTimeCells = cookie => json(get('weekViewAjaxAction.do?oper=getTimeCells', cookie))
