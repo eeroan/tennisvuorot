09431406a94812a60fbc808d5864d29fa1d6147e
Eero Anttila <eea@iki.fi>
2016-10-10 16:56:52 +0300

    Add tests for date handling

--- src/date.js
+++ src/date.js
@@ -1,10 +1,29 @@
-module.exports = { freeSlots: freeSlots}
+var _ = require('lodash')
+module.exports = {
+    freeSlots,
+    minutes,
+    formatTime,
+    isOverlapping
+}
 
 function freeSlots(startTime, endTime, reservations) {
-    return [
-        '6:30',
-        '14:30',
-        '15:30',
-        '21:30'
-    ]
+    const startTimes = _.range(minutes(startTime), minutes(endTime), 60)
+    const reservationTuples = reservations.map(span => span.split('-').map(minutes))
+    return startTimes.filter(startTime =>
+        reservationTuples.every(reservationTuple => !isOverlapping(startTime, reservationTuple))).map(formatTime)
+}
+
+function isOverlapping(startTime, reservation) {
+    return !(startTime >= reservation[1] || startTime + 60 <= reservation[0])
+}
+
+function minutes(hoursAndMinutes) {
+    const splitted = hoursAndMinutes.split(':')
+    return (Number(splitted[0]) * 60) + Number(splitted[1])
+}
+
+function formatTime(minutes) {
+    const m = minutes % 60
+    const h = (minutes - m) / 60
+    return `${h}:${m}`
 }
--- test/date.spec.js
+++ test/date.spec.js
@@ -1,7 +1,7 @@
 const date = require('../src/date')
-const sampleData = [
-    '07:30-8:30',
-    '09:00-10:00',
+const reservations = [
+    '7:30-8:30',
+    '9:00-10:00',
     '10:00-10:30',
     '10:30-11:30',
     '12:00-14:00',
@@ -18,10 +18,31 @@ const availableSlots = [
     '6:30',
     '14:30',
     '15:30',
+    '20:30',
     '21:30'
 ]
+
+describe('format and parse time', () => {
+    it('returns same list', () => {
+        expect(availableSlots
+            .map(date.minutes)
+            .map(date.formatTime)).to.eql(availableSlots)
+    })
+})
+
 describe('date lib', () => {
     it('lists free slots', () => {
-        expect(date.freeSlots(startTime, endTime, sampleData)).to.eql(availableSlots)
+        expect(date.freeSlots(startTime, endTime, reservations)).to.eql(availableSlots)
+    })
+})
+
+describe('is overlapping', () => {
+    it('works as expected', () => {
+        expect(date.isOverlapping(540, [540, 600])).to.be.true
+        expect(date.isOverlapping(570, [540, 600])).to.be.true
+        expect(date.isOverlapping(600, [540, 600])).to.be.false
+        expect(date.isOverlapping(660, [540, 600])).to.be.false
+        expect(date.isOverlapping(date.minutes('8:30'), ['9:00','10:00'].map(date.minutes))).to.be.true
+        expect(date.isOverlapping(date.minutes('8:00'), ['9:00','10:00'].map(date.minutes))).to.be.false
     })
 })
