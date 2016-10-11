6e4a1e22e0207d4b8e361ba1a388d6e7c0029a5e
Eero Anttila <eea@iki.fi>
2016-10-11 22:48:05 +0300

    Rename

--- scripts/updateProfiles.js
+++ scripts/updateProfiles.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const format = require('../src/format')
-const webTimmi2 = require('../src/dao/webTimmi2')
+const webTimmi = require('../src/dao/webTimmi')
 
 const fileName = 'profiles.js'
 
-webTimmi2.getProfiles.onValue(data => fs.writeFileSync(__dirname + '/../generated/' + fileName, format.formatModule(data)))
+webTimmi.getProfiles.onValue(data => fs.writeFileSync(__dirname + '/../generated/' + fileName, format.formatModule(data)))
