b52eb07149639dd97c1ecfb84c1e9bcbd39065cf
Eero Anttila <eea@iki.fi>
2016-10-11 21:56:00 +0300

    Remove dead code

--- test/crawler.spec.js
+++ test/crawler.spec.js
@@ -1,6 +1,5 @@
 var fs = require('fs')
 var slSystems = require('../src/dao/slSystemsCrawler')
-var webTimmi = require('../src/dao/webTimmiCrawler')
 
 describe('sls systems crawler', () => {
     var obj
@@ -8,19 +7,6 @@ describe('sls systems crawler', () => {
     before(() => {
         obj = slSystems.table(fs.readFileSync(__dirname + '/fixture/meilahti.html', 'utf-8'))
         expected = JSON.parse(fs.readFileSync(__dirname + '/fixture/meilahti.json', 'utf-8'))
-    })
-
-    it('transforms table to json', () => {
-        expect(obj).to.eql(expected)
-    })
-})
-
-describe('web timmi crawler', () => {
-    var obj
-    var expected
-    before(() => {
-        obj = webTimmi.parseMarkup(fs.readFileSync(__dirname + '/fixture/weekViewMenu.html', 'utf-8'))
-        expected = JSON.parse(fs.readFileSync(__dirname + '/fixture/taivallahti.json', 'utf-8'))
     })
 
     it('transforms table to json', () => {
