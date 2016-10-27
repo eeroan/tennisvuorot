5b103dae3426bf9b19449c421ba85ee0d50d8c1e
Eero Anttila <eea@iki.fi>
2016-10-27 12:24:19 +0300

    Use object spread

--- src/calendarEvent.js
+++ src/calendarEvent.js
@@ -13,24 +13,7 @@ function show(req, res) {
     res.send(getCalendarLink(req.query))
 }
 
-function getCalendarLink(query) {
-    //http://localhost:5000/calendar
-    // ?location=meilahti
-    // &field=Kupla%20K2
-    // &price=19
-    // &tel=050%203748068
-    // &date=2016-01-18
-    // &time=07:30
-    // &address=Meilahden%20Liikuntapuisto,%2000250%20Helsinki
-    // &url=https://www.slsystems.fi/meilahti/
-    const location = query.location//kulosaari
-    const field = query.field//Bolltex Te2
-    const price = query.price//20
-    const tel = query.tel//09%206211303
-    const date = query.date//2016-01-18
-    const time = query.time//08:30
-    const address = query.address//Kulosaarentie%202,%2000570%20Helsinki
-    const url = query.url//http://www.slsystems.fi/puhoscenter/
+function getCalendarLink({location, field, price, tel, date, time, address, url}) {
     const startTime = DateTime.fromIsoDateTime(date + 'T' + time + ':00')
     const duration = 60
     const summary = `Tennisvuoro ${location} ${field}`
