ec1674d48407312b011276ec1670776fa2bd7d64
Eero Anttila <eea@iki.fi>
2019-12-27 20:52:01 +0200

    Extract constants

--- worker.js
+++ worker.js
@@ -1,15 +1,17 @@
 const DateTime = require('dateutils').DateTime
 const dao = require('./src/dao/dao')
 
+const intervalInSeconds = 60 * 5
+const seconsAfterFirstRefresh = 120
 updateReservations(2)
-setTimeout(() => updateReservations(60), 1000 * 60 * 2)
+setTimeout(() => updateReservations(60), 1000 * seconsAfterFirstRefresh)
 
 function updateReservations(maxDaysAhead) {
     let daysAhead = 0
     doRefresh()
     setInterval(() => {
         doRefresh()
-    }, 1000 * 60 * 5)
+    }, 1000 * intervalInSeconds)
 
     function doRefresh() {
         dao.refresh(new DateTime().plusDays(daysAhead).toISODateString(), 1, () => { })
