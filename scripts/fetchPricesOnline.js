ce213058492d2b356123d35f0156a397dddf4364
Eero Anttila <eea@iki.fi>
2019-12-30 13:07:33 +0200

    Add skeleton for fetching prices online

---
+++ scripts/fetchPricesOnline.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+// wget -q -O - "https://meilahti.slsystems.fi/booking/create-booking?alkuaika=2019-12-31+14:00:00&kesto=60&resid=7" | grep €
+console.log('Not implemented yet!')
