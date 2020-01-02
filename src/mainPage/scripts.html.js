6daac1729d8c60b4b617eb9ef5f253abf9b39df4
Eero Anttila <eea@iki.fi>
2020-01-02 21:34:31 +0200

    Port maps to openstreetmap

--- src/mainPage/scripts.html.js
+++ src/mainPage/scripts.html.js
@@ -4,7 +4,9 @@ ${o.isTest ? '<script src="//cdnjs.cloudflare.com/ajax/libs/classlist/2014.01.31
     window.refresh=${o.refresh};
     window.serverDate='${o.serverDate}';
 </script>
-<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+
 <script>
     (function (i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
--- src/mapView.js
+++ src/mapView.js
@@ -7,29 +7,27 @@ module.exports = {
 const locationTooltip = location => `<h3>${location.title}</h3>${linksMarkup(location)}`
 
 function renderMap() {
-    const bounds = new google.maps.LatLngBounds()
-    const map = new google.maps.Map(document.getElementById('map_canvas'), {mapTypeId: 'roadmap'})
-   // map.setTilt(45)
-    const infoWindow = new google.maps.InfoWindow()
-    locations.filter(function (loc) {
-        return loc.lat !== null
-    }).forEach((loc, i) => {
-        const position = new google.maps.LatLng(loc.lat, loc.lng)
-        bounds.extend(position)
-        const marker = new google.maps.Marker({
-            position: position,
-            map:      map,
-            title:    loc.title,
-            icon:     '/tennisvuorot-marker.png'
-        })
-        google.maps.event.addListener(marker, 'click', ((marker, i) =>
-            () => {
-                infoWindow.setContent(locationTooltip(loc))
-                infoWindow.open(map, marker)
-            }
-        )(marker, i))
-        map.fitBounds(bounds)
+    const map = L.map('map_canvas').fitWorld()
+    L.tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+        {
+            maxZoom: 18,
+            id: 'mapbox/streets-v11'
+        }).addTo(map)
+    const LeafIcon = L.Icon.extend({
+        options: {
+            iconSize: [29, 36],
+            iconAnchor: [15, 36],
+            popupAnchor: [0, -20],
+            shadowUrl: 'marker-shadow.png',
+            shadowSize: [67, 66],
+            shadowAnchor: [20, 65]
+        }
     })
+    const icon = new LeafIcon({iconUrl: 'tennisvuorot-marker.png'})
+    const group = new L.featureGroup(locations.filter(loc => !isNaN(loc.lat)).map((loc, i) =>
+        L.marker(loc, {icon}).addTo(map).bindPopup(locationTooltip(loc))))
+    map.fitBounds(group.getBounds())
 }
 
 
@@ -37,7 +35,7 @@ function linksMarkup(locationObject) {
     const address = locationObject.address
     const url = locationObject.url
     const tel = locationObject.tel
-    const systemLink = (url ? `<div><a target="_blank" href="${url}">Siirry constausjärjestelmään</a></div>` : '')
+    const systemLink = (url ? `<div><a target="_blank" href="${url}">Siirry varausjärjestelmään</a></div>` : '')
     const addressLink = `<div><a class="map" target="_blank" href="http://maps.google.com/?q=${address}">${address}</a></div>`
     const telLink = `<div><a class="tel" href="tel:${tel}">${tel}</a></div>`
     return `<div class="links">${telLink}${addressLink}${systemLink}</div>`
