22d05eeb0a0536f0cdeac0d2ad56e40554b020df
Eero Anttila <eea@iki.fi>
2020-02-01 17:47:20 +0200

    Update code for fetching prices

--- scripts/fetchPricesOnline.js
+++ scripts/fetchPricesOnline.js
@@ -1,65 +1,97 @@
 #!/usr/bin/env node
 const request = require('request')
 const util = require('util')
-var DateTime = require('dateutils').DateTime
-var DateLocale = require('dateutils').DateLocale
+const DateTime = require('dateutils').DateTime
+const DateLocale = require('dateutils').DateLocale
+const _ = require('lodash')
+const fs = require('fs').promises
 
-const urls = {
-    taivallahti: date => `https://varaukset.talintenniskeskus.fi/booking/create-booking?alkuaika=${date}&kesto=60&resid=46`,
-    tali:        {
-        indoor: date => `https://varaukset.talintenniskeskus.fi/booking/create-booking?alkuaika=${date}&kesto=60&resid=1`
-    },
-    kulosaari:   {
-        indoor: date => `https://puhoscenter.slsystems.fi/booking/create-booking?alkuaika=${date}&kesto=60&resid=1`,
-        bubble: date => `https://puhoscenter.slsystems.fi/booking/create-booking?alkuaika=${date}&kesto=60&resid=3`,
+function formatDateTime(dateTime) {
+    return dateTime.toString().replace('T', '+');
+}
 
-    },
-    meilahti:    {
-        indoor: date => `https://meilahti.slsystems.fi/booking/create-booking?alkuaika=${date}&kesto=60&resid=6`,
-        bubble: date => `https://meilahti.slsystems.fi/booking/create-booking?alkuaika=${date}&kesto=60&resid=2`
-        },
-    herttoniemi: { // vanha
-        indoor: date => `https://www.slsystems.fi/fite/ftpages/ft-varaus-input-01.php?laji=1&res=4&pvm=2020-01-05&klo=21:00:00&kesto=01:00:00`,
-        bubble: date => `https://www.slsystems.fi/fite/ftpages/ft-varaus-input-01.php?laji=1&res=12&pvm=2020-01-05&klo=21:00:00&kesto=01:00:00`
-    }, // vanha
-    merihaka:    date => `https://www.slsystems.fi/meripeli/ftpages/ft-varaus-input-01.php?laji=3&res=26&pvm=2020-01-23&klo=08:30:00&kesto=01:00:00`,
-    // vanha
-    tapiola:     date => `https://www.slsystems.fi/tennispuisto/ftpages/ft-varaus-input-01.php?laji=1&res=3&pvm=2020-01-16&klo=08:30:00&kesto=01:00:00`,
-    // vanha
-    laajasalo:   date => `https://www.slsystems.fi/laajasalonpalloiluhallit/ftpages/ft-varaus-input-01.php?laji=1&res=1&pvm=2020-01-05&klo=22:00:00&kesto=01:00:00`,
-    hiekkaharju: {indoor: 1018267466, bubble: 692647002}
+function formatDateTimeOld(dateTime) {
+    return dateTime.toString().replace('T', '&klo=');
 }
 
 const parsePrice = markup => {
-    return parseInt(markup.match(/.*<td>(\d+)\s*€/)[1], 10)
+    return Number(markup.match(/.*(\d+)\s*€/)[1])
+}
+const parsePriceOld = markup => {
+    try {
+        return Number(markup.match(/.*Hinta.*>([\d|.]+)\s+<.*/)[1])
+    } catch (e) {
+        process.stdout.write(markup)
+        return -1
+    }
+}
+const urls = {
+    taivallahti: [date => `https://varaukset.talintenniskeskus.fi/booking/create-booking?alkuaika=${formatDateTime(date)}&kesto=60&resid=46`, parsePrice],
+    tali: {
+        indoor: [date => `https://varaukset.talintenniskeskus.fi/booking/create-booking?alkuaika=${formatDateTime(date)}&kesto=60&resid=1`, parsePrice]
+    },
+    kulosaari: {
+        indoor: [date => `https://puhoscenter.slsystems.fi/booking/create-booking?alkuaika=${formatDateTime(date)}&kesto=60&resid=1`, parsePrice],
+        bubble: [date => `https://puhoscenter.slsystems.fi/booking/create-booking?alkuaika=${formatDateTime(date)}&kesto=60&resid=3`, parsePrice],
+    },
+    meilahti: {
+        indoor: [date => `https://meilahti.slsystems.fi/booking/create-booking?alkuaika=${formatDateTime(date)}&kesto=60&resid=6`, parsePrice],
+        bubble: [date => `https://meilahti.slsystems.fi/booking/create-booking?alkuaika=${formatDateTime(date)}&kesto=60&resid=2`, parsePrice]
+    },
+    herttoniemi: { // vanha
+        indoor: [date => `https://www.slsystems.fi/fite/ftpages/ft-varaus-input-01.php?laji=1&res=4&pvm=${formatDateTimeOld(date)}&kesto=01:00:00`, parsePriceOld],
+        bubble: [date => `https://www.slsystems.fi/fite/ftpages/ft-varaus-input-01.php?laji=1&res=12&pvm=${formatDateTimeOld(date)}&kesto=01:00:00`, parsePriceOld]
+    }, // vanha
+    merihaka: [date => `https://www.slsystems.fi/meripeli/ftpages/ft-varaus-input-01.php?laji=3&res=26&pvm=${formatDateTimeOld(date)}&kesto=01:00:00`, parsePriceOld],
+    // vanha
+    tapiola: [date => `https://www.slsystems.fi/tennispuisto/ftpages/ft-varaus-input-01.php?laji=1&res=3&pvm=${formatDateTimeOld(date)}&kesto=01:00:00`, parsePriceOld],
+    // vanha
+    laajasalo: [date => `https://www.slsystems.fi/laajasalonpalloiluhallit/ftpages/ft-varaus-input-01.php?laji=1&res=1&pvm=${formatDateTimeOld(date)}&kesto=01:00:00`, parsePriceOld],
+    hiekkaharju: {indoor: 1018267466, bubble: 692647002}
 }
 
-const getFor = async (dateTime) => {
-    const resid = 7
-    const url = `https://meilahti.slsystems.fi/booking/create-booking?alkuaika=${dateTime.toString().replace('T', '+')}&kesto=60&resid=${resid}`
-    console.log('Getting price for ', url)
-    const res = await util.promisify(request.get)({
-        jar: true,
-        url
-    })
-    return parsePrice(res.body)
+
+const getFor = async ([urlFunc, parser], dateTime) => {
+    const url = urlFunc(dateTime)
+    process.stdout.write(url + ' → ')
+    try {
+        const res = await util.promisify(request.get)({
+            jar: true,
+            url
+        })
+        const price = parser(res.body)
+        console.log(price)
+        return price
+    } catch (e) {
+        console.log(e.message)
+        return ''
+    }
 }
 //su=0
 
 const range = size => [...Array(size).keys()]
-const main = async () => {
-    const start = DateTime.today().plusDays(7).getFirstDateOfWeek(DateLocale.FI)
+const table = async key => {
+    const start = DateTime.today().plusDays(90).getFirstDateOfWeek(DateLocale.FI)
     const times = {}
     for (let x in range(34)) {
         const time = (60 + (x * 5))
         const dates = []
         for (let x2 in range(6)) {
-            const date = await getFor(start.plusMinutes((6 * 60) + (x * 30)).plusDays(x2))
+            const date = await getFor(key, start.plusMinutes((6 * 60) + (x * 30)).plusDays(x2))
             dates.push(date.toString())
         }
         times[time] = dates
+        return times
     }
-    console.log(times)
 }
+
+
+const main = async (key) => {
+
+
+await fs.writeFile('rates2.js', util.inspect(times, null, 2))
+console.log(times)
+}
+
 
 main()
